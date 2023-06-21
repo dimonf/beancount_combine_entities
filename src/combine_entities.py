068696f2d6bfa653c384cd1d9dc876aa4e5691b5
dimonf <camel109@gmail.com>
2023-06-22 01:22:30 +0300

    config change. more elaborate error handling
      new feature: meta value in output can be derived from the source data

--- src/combine_entities.py
+++ src/combine_entities.py
@@ -1,30 +1,56 @@
 """Combine transactions of two separate entities that have transactions between
-   each other. For example, the principal pays money to the agent, which, in turn,
-   spends funds according to the principal's instructions. To avoid recording 
-   such a spending in books of both companies, an accountant can combine records 
-   of both companies with "include" directive, and then, use this plugin to
-   transform transactions of interest in books of agent for needs of the principal 
-   Special meta value with key '_meta' has this format:
-     "<account>;<meta_n>"
-    e.g:
-     "Expenses:Agency; sub:sales expenses; com: advance and arrears"
-    which will be transformed into a balancing posting for our_account:
+   each other. For example, the principal pays money to the agent, which then
+   spends funds according to the principal's instructions. If beancount datafiles
+   for both companies available, such spendings can be recorder in books of the
+   agent only. A third bean file then can be created, where
+     - both "importing" and "exporting" files are sourced by "include" directive,
+     - plugin directive's config determins selection and transformation of 
+       transactions
+    In general, all entries of Transaction type are ignored if:
+     - they do not have tag 'our_tag', and
+     - they do not have postings specified by 'filter_account'
+    Postings with account matching 'filter_account', 'filter_amount' are transformed
+    (most probably, altering sign of the amount, if 'invert_amount' is True), 
+    padded with corresponding posting to balance transaction. This balancing
+    posting is constructed, based on two-stage mechanism:
+     - 'exporting' posting has a meta with 'super_meta' key, which will be
+        left intact and be copied into the merged data set
+     - plugin's config shall specify both, this 'super_meta' name, and mapping
+       for this super_meta values, between "exporting" entries and "importing"
+       dataset. The "importing" side of this mapping is a string of the following
+       structure:
+         'sm_<from_meta_name>':"<account>;<meta_name:meta_value>', 
+        eg:
+         'sm_sales': 'Expenses:Agency; sub:sales expenses, com: advance and arrears'
+       Value of new meta can be set to '*', in which case the value of 'exporting' 
+         meta will be used
+
+    The previous example config will be transformed into a balancing posting for 
+    our_account:
 
       Assets:Agent         -500.00 EUR
+         sub: "sales"
       Expenses:Agency 
          sub: "sales expenses"
          com: "advance and arrears"
 
+    This mapping allows for certain level of decoupling between two data files
 
    include "agent_records.bean"
-   plugin "combine_entities" "{'filter_account' = 'Liabilities:Principal', \
-                               'our_tag'         = "oi-master",\
-                               'filter_flag'     = "x", \
-                               'filter_amount'   = 'dt', \
-                               'invert_amount'   = True, \
-                               'our_account'     = 'Assets:Agent', \
-                               'super_meta'      = 's_meta', \
-                               }"
+   include "principal_records.bean"
+   plugin "combine_entities" "{
+             'filter_account'  : 'Liabilities:Principal', 
+             'our_tag'         : "oi-master",
+             'filter_flag'     : "x", 
+             'filter_amount'   : 'dt',
+             'invert_amount'   : True,
+             'our_account'     : 'Assets:Agent',
+             'super_meta'      : 'sub',
+             'sm_sales'        : 'Expenses:Agency;sub:sales expenses;com:advance',
+             'sm_cash*'        : 'Liabilities:Intra-group;sub:*',
+             }"
+    
+    Filter by flag is yet to be implemented
 """
 
 __version__ = "0.2"
@@ -33,6 +59,7 @@ __license__ = "MIT License"
 __plugins__ = ('combine_entities',)
 
 import re
+from fnmatch import fnmatch
 
 from beancount.core import data
 from beancount.parser import options
@@ -52,7 +79,6 @@ def combine_entities(entries, options_map, config_str):
     config = eval(config_str, {}, {})
     if not isinstance(config, dict):
         raise RuntimeError("Invalid plugin configuration: args must be a single dictionary")
-    #print(config) #DELME
 
     config['filter_flag'] = config.get('filter_flag', filter_flag)
     config['super_meta'] = config.get('super_meta', super_meta)
@@ -62,6 +88,16 @@ def combine_entities(entries, options_map, config_str):
     our_files = {} #file path(s) for files, where use of our_tag was detected
     errors = []
 
+    #parse config
+    config['meta_map'] = config.get('meta_map', {})
+    for k,v in config.items():
+        if k.startswith('sm_'):
+            acc, *meta = v.split(';')
+            config['meta_map'][k[3:]] = {
+                'account':acc,
+                'meta'   :{a[0]:a[1] for a in (b.split(':') for b in meta)}
+            }
+
     for entry in entries:
         if isinstance(entry, data.Transaction):
             if config['our_tag'] in entry.tags:
@@ -70,13 +106,12 @@ def combine_entities(entries, options_map, config_str):
             else:
                 #here go only "foreign" entries
                 entry, replaced = replace_entry(entry, config)
-                #print(replaced)
                 if replaced:
                     new_t_entries.append(entry)
         else:
             new_n_entries.append(entry)
 
-    #remove entries of all types other than Transaction from "foreign" files
+    #drop entries of all types other than Transaction from "foreign" files
     for entry in new_n_entries:
         if entry.meta['filename'] in our_files:
             new_t_entries.append(entry)
@@ -93,32 +128,53 @@ def replace_entry(entry, config):
             not test_amount(posting.units, config):
                 continue
 
-        meta_new = posting.meta.copy()
         #super_meta = "account; sub: Sales expenses; com: Reimburseable; ..."
         try:
-            super_meta = meta_new.pop(config['super_meta']).split(';')
+            sm_val = posting.meta[config['super_meta']]
         except KeyError:
-            message = """please write proper _meta for posting:
-            {}:{}""".format(meta_new['filename'],meta_new['lineno'])
+            message = """key {} is not defined for entry at
+            {}:{}""".format(config['super_meta'],
+                            posting.meta['filename'],
+                            posting.meta['lineno'],
+                            )
             print(message)
-            #raise KeyError(message)
             continue
 
+        bal_p = None
+        try:
+            bal_p = config['meta_map'][sm_val]
+        except KeyError:
+            for k in config['meta_map'].keys():
+                if fnmatch(sm_val, k):
+                    bal_p = config['meta_map'][k]
+                    break
+            if not bal_p:
+                message = """please write proper config for meta {}:{} at
+                {}:{}""".format(config['super_meta'],
+                                sm_val,
+                                posting.meta['filename'],
+                                posting.meta['lineno'],
+                                )
+                print(message)
+                #raise KeyError(message)
+                continue
+
+            #replace * with value from "exporting" posting meta
+            for k,v in bal_p['meta'].items():
+                if v == '*':
+                    bal_p['meta'][k] = sm_val
+
         new_posting = posting._replace(account = config['our_account'],
-                                       meta = meta_new,
                                        units = -posting.units)
         new_postings.append(new_posting)
         #balancing posting
-        n_account = super_meta.pop(0).strip()
-        n_meta = {v[0]:f'"{v[1]}"' for v in [s.split(':') for s in super_meta]}
-        #Posting(account, units, cost, price, flag, meta)
         bal_posting = data.Posting(
-            account = n_account,
+            account = bal_p['account'],
             units   = posting.units,
             cost    = None,
             price   = None,
             flag    = None,
-            meta    = n_meta
+            meta    = bal_p['meta']
         )
         new_postings.append(bal_posting)
     if new_postings:
